f0d5cf007d66a205ce20f7ad7af3dad8a7b962be
srdoyen <srdoyen@gmail.com>
2019-12-16 20:13:17 -0800

    ui

--- src/app.js
+++
@@ -1,9 +0,0 @@
-//Common JS Module Syntax
-//const person = require("./mymodule1");
-//import { person } from "./mymodule2";
-
-//import * as mod from "./mymodule2";
-import greeting from "./mymodule2";
-//console.log(mod.person.name);
-//console.log(mod.sayHello());
-console.log(greeting);
